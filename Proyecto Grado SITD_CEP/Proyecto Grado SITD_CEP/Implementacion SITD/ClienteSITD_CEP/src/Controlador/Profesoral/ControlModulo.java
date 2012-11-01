068a95657d10d876de4c4af76f7814d4da903cf6
Camilo Ortiz <yeskmilo@gmail.com>
2012-10-31 22:51:40 -0500

    Creando nueva vista modulos y controlador

---
+++ Proyecto Grado SITD_CEP/Proyecto Grado SITD_CEP/Implementacion SITD/ClienteSITD_CEP/src/Controlador/Profesoral/ControlModulo.java
@@ -0,0 +1,15 @@
+/*
+ * Sistema de Información para la toma de Decisiones
+ * Centro de Educación Permanente
+ * Autor: 
+ * Yesid Camilo Ortiz Castillo 
+ */
+package Controlador.Profesoral;
+
+/**
+ *
+ * @author juanpanlo
+ */
+public class ControlModulo {
+    
+}
